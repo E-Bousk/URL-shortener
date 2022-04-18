c6d0d662eb7de638299c848d3b58b58caed5dc25
E.Bousk <e.bousk@gmail.com>
2022-04-18 12:41:27 +0200

    Add test for form logic

--- tests/Controller/UrlsControllerTest.php
+++ tests/Controller/UrlsControllerTest.php
@@ -6,10 +6,11 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class UrlsControllerTest extends WebTestCase
 {
-    public function testHomepageShouldDisplayUrlShortenerForm(): void
+    /** @test */
+    public function homepage_should_display_url_shortener_form(): void
     {
         $client = static::createClient();
-        $crawler = $client->request('GET', '/');
+        $client->request('GET', '/');
 
         $this->assertResponseIsSuccessful();
         $this->assertPageTitleSame('URL Shortener');
@@ -18,5 +19,27 @@ class UrlsControllerTest extends WebTestCase
         $this->assertSelectorExists('input[id="form_original"]');
         $this->assertSelectorExists('input[name="form[original]"]');
         $this->assertSelectorExists('input[placeholder="Enter the URL to shorten here"]');
+    }
+
+    /** @test */
+    public function form_should_work_with_valid_data()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+
+        $this->assertResponseIsSuccessful();
+
+        // Avec un bouton de soumission (voir « create.html.twig ») :
+        // $client->submitForm('Shorten URL', [
+        //     'form[original]' => 'https://symfony.com'
+        // ]);
+
+        // Sans bouton de soumission :
+        $form = $crawler->filter('form')->form();
+        $client->submit($form, [
+            'form[original]' => 'https://symfony.com'
+        ]);
+
+        $this->assertResponseRedirects();
     }
 }
--- tests/Util/StrTest.php
+++ tests/Util/StrTest.php
@@ -7,18 +7,9 @@ use PHPUnit\Framework\TestCase;
 
 class StrTest extends TestCase
 {
-    // (avec « testQuelquechose »)
     public function testRandomStringLength(): void
     {
-        // $this->assertTrue(mb_strlen(Str::random(6)) === 6);
         $this->assertSame(6, mb_strlen(Str::random(6)));
-    }
-
-    // (avec annotation « test »)
-    /** @test */
-    public function randomStringLength(): void
-    {
-        // $this->assertTrue(mb_strlen(Str::random()) === 16);
         $this->assertSame(16, mb_strlen(Str::random()));
     }
 }
